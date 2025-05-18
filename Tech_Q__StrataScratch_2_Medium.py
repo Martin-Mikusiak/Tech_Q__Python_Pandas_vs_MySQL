8a8514c399fabfd0309a75df7449bbb664203a2d
Martin Mikušiak <mikusiak.martin@gmail.com>
2025-05-19 00:12:08 +0200

    Create Tech_Q__StrataScratch_2_Medium.py

---
+++ Tech_Q__StrataScratch_2_Medium.py
@@ -0,0 +1,559 @@
+# StrataScratch - Technical Questions (only the "Free" ones)
+# **********************************************************
+# https://platform.stratascratch.com/coding?code_type=2&is_freemium=1
+
+# My own solutions - both in Python Pandas and in MySQL
+
+# ***** In progress *****
+
+
+# Contents
+
+# 1. Difficulty: Easy  (27 Questions)
+# See the previous file "Tech_Q__StrataScratch_1_Easy.py"
+
+# 2. Difficulty: Medium  (41 Questions)
+#    2.1 Share of Active Users
+#    2.2 Premium Accounts
+#    2.3 Election Results
+#    2.4 Flags per Video
+#    2.5 User with Most Approved Flags
+#    2.6 Find Students At Median Writing
+#    2.7 Top 10 Songs 2010
+#    2.8 Classify Business Type
+#    2.9 Processed Ticket Rate By Type
+#    2.10 Customer Revenue In March
+#    2.11 Count Occurrences Of Words In Drafts
+#    2.12 Titanic Survivors and Non-Survivors
+#    2.13 Second Highest Salary
+#    2.14 Employee and Manager Salaries
+#    2.15 ***** In progress *****
+#    2.16 
+#    2.17 
+
+# 3. Difficulty: Hard  (12 Questions)
+#    3.1 ***** In progress *****
+#    3.2 
+#    3.3 
+#    3.4 
+
+
+
+# 2. Difficulty: Medium  (41 Questions)
+# *************************************
+
+# 2.1 Share of Active Users
+# https://platform.stratascratch.com/coding/2005-share-of-active-users?code_type=2
+
+# Calculate the percentage of users who are both from the US and have an 'open' status, as indicated in the fb_active_users table.
+
+
+# Python
+# ******
+import pandas as pd
+
+usa_active_pctg = fb_active_users[
+    fb_active_users["country"].eq("USA") &
+    fb_active_users["status"].eq("open")
+].shape[0] / fb_active_users.shape[0] * 100
+
+
+# MySQL
+# *****
+SELECT
+    COUNT(*) / (SELECT COUNT(*) FROM fb_active_users) * 100 AS usa_active_pctg
+FROM fb_active_users
+WHERE
+    country = 'USA' AND
+    status  = 'open';
+
+
+
+# 2.2 Premium Accounts
+# https://platform.stratascratch.com/coding/2097-premium-acounts?code_type=2
+
+# You have a dataset that records daily active users for each premium account.
+# A premium account appears in the data every day as long as it remains premium.
+# However, some premium accounts may be temporarily discounted, meaning they are not actively paying — this is indicated by a final_price of 0.
+# For each of the first 7 available dates, count the number of premium accounts that were actively paying on that day.
+# Then, track how many of those same accounts remain premium and are still paying exactly 7 days later (regardless of activity in between).
+# Output three columns:
+# - The date of initial calculation.
+# - The number of premium accounts that were actively paying on that day.
+# - The number of those accounts that remain premium and are still paying after 7 days.
+
+
+# Python
+# ******
+import pandas as pd
+
+df = premium_accounts_by_day.assign(date_p_7d = premium_accounts_by_day["entry_date"] + pd.DateOffset(7))
+
+dfm = df.merge(df, how="left", left_on=["account_id", "date_p_7d"], right_on=["account_id", "entry_date"], suffixes=("_L", "_R"))
+dfm = dfm[dfm["final_price_L"].gt(0)][["account_id", "entry_date_L", "final_price_L", "final_price_R"]]
+dfm = dfm.rename(columns={"entry_date_L": "entry_date"})
+
+dfm_gr = dfm.groupby(by="entry_date", as_index=False).agg(
+    premium_paid_acc_count         =("final_price_L", "count"),
+    premium_paid_acc_count_after_7d=("final_price_R", lambda x: x.gt(0).sum())
+    ).head(7)
+
+
+# MySQL
+# *****
+SELECT
+    p1.entry_date,
+    COUNT(p1.final_price)            AS premium_paid_acc_count,
+    COUNT(NULLIF(p2.final_price, 0)) AS premium_paid_acc_count_after_7d
+FROM premium_accounts_by_day AS p1
+LEFT JOIN premium_accounts_by_day AS p2
+    ON p1.account_id = p2.account_id AND
+    ADDDATE(p1.entry_date, 7) = p2.entry_date
+WHERE
+    p1.final_price > 0
+GROUP BY p1.entry_date
+LIMIT 7;
+
+
+
+# 2.3 Election Results
+# https://platform.stratascratch.com/coding/2099-election-results?code_type=2
+
+# The election is conducted in a city and everyone can vote for one or more candidates, or choose not to vote at all.
+# Each person has 1 vote so if they vote for multiple candidates, their vote gets equally split across these candidates.
+# For example, if a person votes for 2 candidates, these candidates receive an equivalent of 0.5 vote each.
+# Some voters have chosen not to vote, which explains the blank entries in the dataset.
+# Find out who got the most votes and won the election. Output the name of the candidate, or multiple names in case of a tie.
+# To avoid issues with a floating-point error you can round the number of votes received by a candidate to 3 decimal places.
+
+
+# Python
+# ******
+import pandas as pd
+
+df = voting_results[
+    voting_results["candidate"].notna() & 
+    voting_results["candidate"].str.strip().ne("")
+    ].sort_values(by=["voter", "candidate"]).reset_index(drop=True)
+
+df = df.assign(vote_value = 1 / df.groupby(by="voter")["candidate"].transform("count"))
+
+df_results = df.groupby(by="candidate", as_index=False)["vote_value"].sum().round(3)
+df_results = df_results.rename(columns={"vote_value": "vote_value_sum"})
+
+df_results = df_results.assign(vote_rank = df_results["vote_value_sum"].rank(method="dense", ascending=False))
+df_results["vote_rank"] = df_results["vote_rank"].astype(int, errors="ignore")              # This line is not required, but nice for presentation of the overall results
+df_results = df_results.sort_values(by=["vote_rank", "candidate"]).reset_index(drop=True)   # This line is not required, but nice for presentation of the overall results
+df_results[df_results["vote_rank"].eq(1)]["candidate"]
+
+
+# MySQL
+# *****
+WITH cte_vote_value AS
+(
+SELECT
+    voter,
+    candidate,
+    1 / COUNT(*) OVER(PARTITION BY voter) AS vote_value
+FROM voting_results
+WHERE
+    TRIM(candidate) != '' AND
+    candidate IS NOT NULL
+ORDER BY voter, candidate
+),
+cte_vote_value_sum AS
+(
+SELECT
+    candidate,
+    ROUND(SUM(vote_value), 3) AS vote_value_sum
+FROM cte_vote_value
+GROUP BY candidate
+),
+cte_rank AS
+(
+SELECT
+    *,
+    DENSE_RANK() OVER(ORDER BY vote_value_sum DESC) AS vote_rank
+FROM cte_vote_value_sum
+)
+SELECT candidate
+FROM cte_rank
+WHERE vote_rank = 1;
+
+
+
+# 2.4 Flags per Video
+# https://platform.stratascratch.com/coding/2102-flags-per-video?code_type=2
+
+# For each video, find how many unique users flagged it.
+# A unique user can be identified using the combination of their first name and last name.
+# Do not consider rows in which there is no flag ID.
+
+
+# Python
+# ******
+import pandas as pd
+
+df = user_flags[user_flags["flag_id"].notna() & user_flags["flag_id"].str.strip().ne("")]
+df = df.assign(user_fullname = df["user_firstname"].fillna("_").str.strip() + " " + df["user_lastname"].fillna("_").str.strip())
+df = df[["video_id", "user_fullname"]].drop_duplicates().sort_values(by=["video_id", "user_fullname"])
+
+df_gr = df.groupby(by="video_id").size().to_frame("num_unique_users").reset_index()
+
+
+# MySQL
+# *****
+WITH cte_fullnames AS
+(
+SELECT
+    video_id,
+    CONCAT( TRIM(COALESCE(user_firstname, '_')), ' ', TRIM(COALESCE(user_lastname, '_')) ) AS user_fullname
+FROM user_flags
+WHERE
+    TRIM(flag_id) != '' AND
+    flag_id IS NOT NULL
+)
+SELECT
+    video_id,
+    COUNT(DISTINCT user_fullname) AS num_unique_users
+FROM cte_fullnames
+GROUP BY video_id
+ORDER BY video_id;
+
+
+
+# 2.5 User with Most Approved Flags
+# https://platform.stratascratch.com/coding/2104-user-with-most-approved-flags?code_type=2
+
+# Which user flagged the most distinct videos that ended up approved by YouTube?
+# Output, in one column, their full name or names in case of a tie.
+# In the user's full name, include a space between the first and the last name.
+
+
+# Python
+# ******
+import pandas as pd
+
+df = user_flags.merge(flag_review, on="flag_id")
+df = df[df["reviewed_outcome"].str.lower().eq("approved")]
+df = df.assign(user_fullname = df["user_firstname"].fillna("_").str.strip() + " " + df["user_lastname"].fillna("_").str.strip())
+df = df[["user_fullname", "video_id"]].drop_duplicates().sort_values(by=["user_fullname", "video_id"])
+
+df_gr = df.groupby(by="user_fullname").size().to_frame("videos_count").reset_index()
+df_gr[df_gr["videos_count"].eq(df_gr["videos_count"].max())]["user_fullname"]
+
+
+# MySQL
+# *****
+WITH cte_appr_vid_count AS
+(
+SELECT
+    CONCAT( TRIM(COALESCE(user_firstname, '_')), ' ', TRIM(COALESCE(user_lastname, '_')) ) AS user_fullname,
+    COUNT(DISTINCT video_id) AS videos_count
+FROM user_flags AS uf
+JOIN flag_review AS fr
+    ON uf.flag_id = fr.flag_id
+WHERE LOWER(reviewed_outcome) = 'approved'
+GROUP BY user_fullname
+)
+SELECT user_fullname
+FROM cte_appr_vid_count
+WHERE videos_count = (SELECT MAX(videos_count) FROM cte_appr_vid_count);
+
+
+
+# 2.6 Find Students At Median Writing
+# https://platform.stratascratch.com/coding/9610-find-students-with-a-median-writing-score?code_type=2
+
+# Identify the IDs of students who scored exactly at the median for the SAT writing section.
+
+
+# Python
+# ******
+import pandas as pd
+
+sat_scores[sat_scores["sat_writing"].eq(sat_scores["sat_writing"].median())]["student_id"]
+
+
+# MySQL
+# *****
+# Unlike Python Pandas, MySQL has no built-in MEDIAN() function.
+WITH cte_row_nr AS
+(
+SELECT
+    ROW_NUMBER() OVER(ORDER BY sat_writing) AS row_nr,
+    student_id,
+    sat_writing,
+    COUNT(*) OVER() AS rows_count
+FROM sat_scores
+),
+cte_median AS
+(
+SELECT
+    AVG(sat_writing) AS s_w_median
+FROM cte_row_nr
+WHERE row_nr IN( FLOOR((rows_count + 1) / 2), CEIL((rows_count + 1) / 2) )
+)
+SELECT student_id
+FROM cte_row_nr, cte_median
+WHERE sat_writing = s_w_median;
+
+
+
+# 2.7 Top 10 Songs 2010
+# https://platform.stratascratch.com/coding/9650-find-the-top-10-ranked-songs-in-2010?code_type=2
+
+# Find the top 10 ranked songs in 2010. Output the rank, group name, and song name, but do not show the same song twice.
+# Sort the result based on the rank in ascending order.
+
+
+# Python
+# ******
+import pandas as pd
+
+df = billboard_top_100_year_end[
+    billboard_top_100_year_end["year"].eq(2010) & 
+    billboard_top_100_year_end["year_rank"].between(1, 10, inclusive="both")
+    ][["year_rank", "group_name", "song_name"]].drop_duplicates().sort_values(by="year_rank")
+
+
+# MySQL
+# *****
+SELECT DISTINCT
+    year_rank,
+    group_name,
+    song_name
+FROM billboard_top_100_year_end
+WHERE
+    year = 2010 AND
+    year_rank BETWEEN 1 AND 10
+ORDER BY year_rank;
+
+
+
+# 2.8 Classify Business Type
+# https://platform.stratascratch.com/coding/9726-classify-business-type?code_type=2
+
+# Classify each business as either a restaurant, cafe, school, or other.
+# - A restaurant should have the word 'restaurant' in the business name.
+# - A cafe should have either 'cafe', 'café', or 'coffee' in the business name.
+# - A school should have the word 'school' in the business name.
+# - All other businesses should be classified as 'other'.
+# Ensure each business name appears only once in the final output.
+# If multiple records exist for the same business, retain only one unique instance.
+# The final output should include only the distinct business names and their corresponding classifications.
+
+
+# Python
+# ******
+import pandas as pd
+
+sf_restaurant_health_violations = sf_restaurant_health_violations.assign(business_type = "other")
+
+df = sf_restaurant_health_violations[["business_name", "business_type"]].drop_duplicates()
+
+# Due to an outdated Pandas version on the StrataScratch server, it was not possible to apply the .case_when() method
+df["business_type"] = df["business_type"].mask(df["business_name"].str.contains("restaurant", case=False, regex=False), "restaurant")
+df["business_type"] = df["business_type"].mask(df["business_name"].str.contains("cafe",       case=False, regex=False), "cafe")
+df["business_type"] = df["business_type"].mask(df["business_name"].str.contains("café",       case=False, regex=False), "cafe")
+df["business_type"] = df["business_type"].mask(df["business_name"].str.contains("coffee",     case=False, regex=False), "cafe")
+df["business_type"] = df["business_type"].mask(df["business_name"].str.contains("school",     case=False, regex=False), "school")
+df
+
+
+# MySQL
+# *****
+SELECT DISTINCT
+    business_name,
+    CASE
+        WHEN business_name LIKE '%restaurant%' THEN 'restaurant'
+        WHEN business_name LIKE '%cafe%'       THEN 'cafe'
+        WHEN business_name LIKE '%café%'       THEN 'cafe'
+        WHEN business_name LIKE '%coffee%'     THEN 'cafe'
+        WHEN business_name LIKE '%school%'     THEN 'school'
+        ELSE 'other'
+    END AS business_type
+FROM sf_restaurant_health_violations;
+
+
+
+# 2.9 Processed Ticket Rate By Type
+# https://platform.stratascratch.com/coding/9781-find-the-rate-of-processed-tickets-for-each-type?code_type=2
+
+# Find the processed rate of tickets for each type.
+# The processed rate is defined as the number of processed tickets divided by the total number of tickets for that type.
+# Round this result to two decimal places.
+
+
+# Python
+# ******
+# Solution #1 - Simple
+import pandas as pd
+
+df = facebook_complaints.groupby(by="type", as_index=False).agg(processed_rate = ("processed", "mean")).round(2)
+
+
+# Solution #2
+import pandas as pd
+
+df = facebook_complaints.groupby(by="type", as_index=False).agg(
+    pr_true  = ("processed"   , "sum"  ),
+    pr_count = ("complaint_id", "count"))
+
+df = df.assign(processed_rate = (df["pr_true"] / df["pr_count"]).round(2))
+df[["type", "processed_rate"]]
+
+
+# MySQL
+# *****
+SELECT
+    type,
+    ROUND(AVG(processed), 2) AS processed_rate
+FROM facebook_complaints
+GROUP BY type;
+
+
+
+# 2.10 Customer Revenue In March
+# https://platform.stratascratch.com/coding/9782-customer-revenue-in-march?code_type=2
+
+# Calculate the total revenue from each customer in March 2019. Include only customers who were active in March 2019.
+# Output the revenue along with the customer id and sort the results based on the revenue in descending order.
+
+
+# Python
+# ******
+import pandas as pd
+
+df = orders[
+    orders["order_date"].dt.strftime("%Y%m").eq("201903")
+    ].groupby(by="cust_id", as_index=False).agg(total_revenue = ("total_order_cost", "sum")).sort_values(by="total_revenue", ascending=False)
+
+
+# MySQL
+# *****
+SELECT
+    cust_id,
+    SUM(total_order_cost) AS total_revenue
+FROM orders
+WHERE EXTRACT(YEAR_MONTH FROM order_date) = 201903
+GROUP BY cust_id
+ORDER BY total_revenue DESC;
+
+
+
+# 2.11 Count Occurrences Of Words In Drafts
+# https://platform.stratascratch.com/coding/9817-find-the-number-of-times-each-word-appears-in-drafts?code_type=2
+
+# Find the number of times each word appears in the contents column across all rows in the drafts dataset.
+# Output two columns: word and occurrences.
+
+
+# Python
+# ******
+import pandas as pd
+
+df_words = google_file_store["contents"].str.lower().str.split().explode().str.replace(r"[.,]", "", regex=True).to_frame("word")
+
+df_gr = df_words.groupby(by="word").size().to_frame("occurrences").reset_index().sort_values(by=["occurrences", "word"], ascending=[False, True])
+
+
+# MySQL
+# *****
+# Two possible complicated solutions:
+# 1. Using a RECURSIVE CTE and SUBSTRING_INDEX() & SUBSTRING() functions, UNION ALL etc. + REGEXP_REPLACE(word, '[.,]', ''), or
+# 2. Using ... JOIN JSON_TABLE() etc. + REGEXP_REPLACE()
+
+
+
+# 2.12 Titanic Survivors and Non-Survivors
+# https://platform.stratascratch.com/coding/9881-make-a-report-showing-the-number-of-survivors-and-non-survivors-by-passenger-class?code_type=2
+
+# Make a report showing the number of survivors and non-survivors by passenger class.
+# Classes are categorized based on the pclass column value.
+# Output the number of survivors and non-survivors by each class.
+
+
+# Python
+# ******
+# Solution #1 - Using Pandas.crosstab() - Simple
+import pandas as pd
+
+df_pivot = pd.crosstab(index=titanic["survived"], columns=titanic["pclass"]).reset_index().rename(columns={1: "1st_class", 2: "2nd_class", 3: "3rd_class"})
+
+
+# Solution #2 - Using .groupby().size() and then .pivot()
+import pandas as pd
+
+df_gr = titanic.groupby(by=["survived", "pclass"]).size().to_frame("count").reset_index()
+
+df_pivot = df_gr.pivot(index="survived", columns="pclass", values="count")
+df_pivot = df_pivot.reset_index().rename_axis(None, axis=1).rename(columns={1: "1st_class", 2: "2nd_class", 3: "3rd_class"})
+
+
+# MySQL
+# *****
+SELECT
+    survived,
+    SUM( CASE WHEN pclass = 1 THEN 1 END ) AS 1st_class,
+    SUM( CASE WHEN pclass = 2 THEN 1 END ) AS 2nd_class,
+    SUM( CASE WHEN pclass = 3 THEN 1 END ) AS 3rd_class
+FROM titanic
+GROUP BY survived
+ORDER BY survived;
+
+
+
+# 2.13 Second Highest Salary
+# https://platform.stratascratch.com/coding/9892-second-highest-salary?code_type=2
+
+# Find the second highest salary of employees.
+
+
+# Python
+# ******
+import pandas as pd
+
+employee = employee.assign(s_rank = employee["salary"].rank(method="dense", ascending=False))
+second_highest_salary = employee[employee["s_rank"].eq(2)][["salary"]].head(1)
+
+
+# MySQL
+# *****
+WITH cte_s_rank AS
+(
+SELECT
+    salary,
+    DENSE_RANK() OVER(ORDER BY salary DESC) AS s_rank
+FROM employee
+)
+SELECT DISTINCT salary AS second_highest_salary
+FROM cte_s_rank
+WHERE s_rank = 2;
+
+
+
+# 2.14 Employee and Manager Salaries
+# https://platform.stratascratch.com/coding/9894-employee-and-manager-salaries?code_type=2
+
+# Find employees who are earning more than their managers.
+# Output the employee's first name along with the corresponding salary.
+
+
+# Python
+# ******
+import pandas as pd
+
+df = employee.merge(employee, how="left", left_on="manager_id", right_on="id", suffixes=("_L", "_R"))
+df[df["salary_L"].gt(df["salary_R"])][["first_name_L", "salary_L"]]
+
+
+# MySQL
+# *****
+SELECT
+    e1.first_name,
+    e1.salary
+FROM employee AS e1
+LEFT JOIN employee AS e2
+    ON e1.manager_id = e2.id
+WHERE e1.salary > e2.salary;
